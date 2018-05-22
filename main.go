adf6d2095100e268cf5be9d22f4a0e1d1b07c712
JamesWojewoda <jwojewoda1@gmail.com>
2018-05-21 23:16:22 -0400

    final comments

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"os"
 	"github.com/gorilla/mux"
 )
-
+// content that will be returned
 type Message struct {
 	Content string `json:"message"`
 }
@@ -21,14 +21,16 @@ type Response struct {
 type Error struct {
 	Error string `json:"err_msg"`
 }
+
 func main() {
 	f, err := os.OpenFile("/var/log/go-service/app.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.SetOutput(f)
-
+	// we utilize gorilla mux to handle unique requests
 	h := mux.NewRouter()
+	//map is our "temporary database"
 	m := make(map[string]string)
 	h.HandleFunc("/messages", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
@@ -41,6 +43,7 @@ func main() {
 	h.HandleFunc("/messages/{hash}", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
 			getresult := GetHash(w, r, m)
+			//if the result of GetHash has content, then we have a match, if not its blank and serve a 404
 			if getresult != "" {
 				response := Message{Content: getresult}
 				json.NewEncoder(w).Encode(response)
@@ -56,15 +59,16 @@ func main() {
 	log.Fatal(http.ListenAndServeTLS(":5000", "/etc/ssl/certs/localhost.crt", "/etc/ssl/certs/localhost.key", h))
 }
 
+//get the endpoint variable with gorilla mux, and return the string contained within the map entry
+//will be blank if theres no entry name with that particular hash value
 func GetHash(w http.ResponseWriter, r *http.Request, m map[string]string) string{
 	defer r.Body.Close()
-
 	vars := mux.Vars(r)
 	hash := vars["hash"]
 
 	return m[hash]
 }
-
+//post to the endpoint and add a new hash value that maps to a new string
 func PostHash(w http.ResponseWriter, r *http.Request, m map[string]string) string{
 	defer r.Body.Close()
 
