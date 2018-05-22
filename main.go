1ec1c46e0c7d78b0c14ac5c684be6122b981107e
JamesWojewoda <jwojewoda1@gmail.com>
2018-05-21 23:10:19 -0400

    final implementation and logging

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"crypto/sha256"
 	"fmt"
+	"os"
 	"github.com/gorilla/mux"
 )
 
@@ -21,6 +22,12 @@ type Error struct {
 	Error string `json:"err_msg"`
 }
 func main() {
+	f, err := os.OpenFile("/var/log/go-service/app.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(f)
+
 	h := mux.NewRouter()
 	m := make(map[string]string)
 	h.HandleFunc("/messages", func(w http.ResponseWriter, r *http.Request) {
@@ -28,20 +35,22 @@ func main() {
 			response := Response{Digest: PostHash(w, r, m)}
 			w.WriteHeader(201)
 			json.NewEncoder(w).Encode(response)
+			log.Println("Post request 201: " + response.Digest)
 		}
 	})
 	h.HandleFunc("/messages/{hash}", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
-			log.Print(GetHash(w, r, m))
-			if GetHash(w, r, m) != "" {
-				response := Message{Content: GetHash(w, r, m)}
+			getresult := GetHash(w, r, m)
+			if getresult != "" {
+				response := Message{Content: getresult}
 				json.NewEncoder(w).Encode(response)
+				log.Println("Get request 200: " + getresult)
 			} else {
 				response := Error{Error: "Not Found"}
 				w.WriteHeader(404)
 				json.NewEncoder(w).Encode(response)
+				log.Println("Get request 404")
 			}
-			
 		}
 	})
 	log.Fatal(http.ListenAndServeTLS(":5000", "/etc/ssl/certs/localhost.crt", "/etc/ssl/certs/localhost.key", h))
